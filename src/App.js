3699c26804eaf0336e33e33e2b63346747286245
MiXeR54 <max_kray@list.ru>
2020-06-23 17:03:10 +0300

    рабочая версия задачи

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
 
-function App() {
+export default function App() {
+  const [counter, setCount] = useState(0);
+  const [arr, setArray] = useState([]);
+
+  //функция добавляет в массив id элемента и его value сгенерированное рандомным образом от 0 до 100 + увеличивает каунтер на кнопке
+  const addArray = useCallback(
+    () => {
+      setArray([
+        ...arr,
+        //добавляем в существующий массив  id: value и value: random.number
+        { id: arr.length, value: Math.floor(Math.random() * 100) + 1 },
+      ]);
+      setCount(counter + 1);
+    }, //депенденсис
+    [arr, counter]
+  );
+
+  useEffect(
+    () => {
+      const timeout = setTimeout(() => addArray(counter + 1), 1000);
+      //чистим таймаут
+      return () => clearTimeout(timeout);
+    }, // депенденсис
+    [counter, addArray]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addArray}>Инкрементировать: {counter}</button>
+      <div>
+        <ul>
+          {arr.map((e) => (
+            <li key={e.id}>{e.value}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
-export default App;
